dec01b0d42bf6cdd60de339642ecc0e2aa4e4110
Michael Kellsy <mkellsy@gmail.com>
2020-11-13 16:10:12 +0000

    initial commit

---
+++ ui/js/index.js
@@ -0,0 +1,52 @@
+$(function(){
+    const form = $("#connect-form");
+    const select = $("#ssid-select");
+
+    let networks = undefined;
+
+    function showHideEnterpriseSettings() {
+        const security = $(this).find(":selected").attr("data-security");
+
+        if(security === "enterprise") {
+            $("#identity-group").show();
+        } else {
+            $("#identity-group").hide();
+        }
+    }
+    
+    select.change(showHideEnterpriseSettings);
+
+    $.get("/networks", (data) => {
+        if(data.length === 0){
+            $(".before-submit").hide();
+            $("#no-networks-message").removeClass("hidden");
+        } else {
+            networks = JSON.parse(data);
+
+            const ssids = [];
+
+            $.each(networks, (_index, value) => {
+                if (value.ssid && value.ssid !== "" && ssids.indexOf(value.ssid) === -1) {
+                    select.append(
+                        $("<option>").text(value.ssid).attr("value", value.ssid).attr("data-security", value.security)
+                    );
+
+                    ssids.push(value.ssid);
+                }
+            });
+
+            jQuery.proxy(showHideEnterpriseSettings, select)();
+        }
+    }).always(() => {
+        select.formSelect();
+    });
+
+    form.submit((event) => {
+        event.preventDefault();
+
+        $.post("/connect", form.serialize(), () => {
+            $(".before-submit").hide();
+            $("#submit-message").removeClass("hidden");
+        });
+    });
+});
